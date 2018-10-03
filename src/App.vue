dc40d071853bfc3a214fe4b89a00c7d6f9f2a391
Victor Pragana <victormerigue@gmail.com>
2018-10-03 19:33:05 -0300

    add some components
    adding component painel and imagemresponsiva

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,47 @@
 <template>
-  <div>
-    <h1>{{titulo}}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+    <h1 class="centralizado">{{titulo}}</h1>
+
+    <input type="search" placeholder="Filtrar por parte do titulo" class="filtro" @input="filtro = $event.target.value">
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+        <meu-painel :titulo="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+        </meu-painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
 export default {
+
+  components: {
+    'meu-painel' : Painel,
+    'imagem-responsiva' : ImagemResponsiva
+  },
+
   data() {
     return {
       titulo: 'Alurapic',
-      fotos: []
+      fotos: [],
+      filtro: ''
     }
   },
+
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
+  },
+
   created() {
     this.$http.get('http://localhost:3000/v1/fotos')
       .then(res => res.json())
@@ -26,4 +51,23 @@ export default {
 </script>
 
 <style>
+  .centralizado {
+    text-align: center;
+  }
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+  .lista-fotos {
+    list-style: none;
+  }
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .filtro {
+    display: block;
+    width: 100%;
+  }
 </style>
